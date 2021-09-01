d52e77c304100b52546be937f98aabd7da3ce28a
yunzhe99 <liyunzhe@whu.edu.cn>
2021-09-01 11:06:31 +0800

    initial

---
+++ config.py
@@ -0,0 +1,2 @@
+class config:
+    training_set = '/home/lion/yunzhe/TJU-DHD/dhd_traffic/train.txt'  # the dir of the whole training set
---
+++ task_tree.py
@@ -0,0 +1,6 @@
+class task_tree:
+    def __init__(self, index, training_set, model, attribute):
+        self.index = index
+        self.training_set = training_set
+        self.model = model
+        self.attribute = attribute
